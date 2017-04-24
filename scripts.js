cc9d36c338c6d0d8c47f5b81d4c6563010442899
Jack Koppa <hello@jackkoppa.com>
2017-04-23 20:41:32 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,65 @@
+var towers = [];
+var timeCounter = 3;
+
+function initialize(height) {
+	var main = document.getElementById("main");
+	var sizeInc = (100 / 3 - 2) / height;
+	var bgInc = Math.floor((256 - 50) / height);
+	var opacityInc = 2000 / height;	
+	timeCounter = 3;
+	function setOpacity(j) {
+		setTimeout(function() {
+			towers[j].style.opacity = 1;
+		}, (height - j) * opacityInc);	
+	}
+
+	while(main.firstChild) {
+		main.removeChild(main.firstChild);
+	}
+	for (var i = 0; i < height; i++) {
+		towers[i] = document.createElement("div");
+		towers[i].id = i + 1;
+		towers[i].className = "tower";
+		towers[i].style.zIndex = height - i;
+		towers[i].style.opacity = 0;
+		towers[i].setAttribute("data-position","1");			
+		var inner = document.createElement("div");	
+		inner.className = "inner";		
+		inner.style.backgroundColor = "rgb(0,0," + (256 - ((bgInc * i))) + ")";
+		inner.style.height = sizeInc * (i + 1) + "vh";
+		inner.style.width = sizeInc * (i + 1) + "vh";
+		towers[i].appendChild(inner);
+		document.getElementById("main").appendChild(towers[i]);
+		setOpacity(i);
+	} 
+}
+
+function move(tower, endPos) {
+	setTimeout(function() {
+		towers[tower].setAttribute("data-position",endPos);
+	}, 500 * timeCounter);
+	timeCounter++;	
+} 
+
+function hanoi(startPos, endPos, midPos, height) {
+	if (height === 1) {
+		move(0, endPos);
+	} 
+	else {
+		hanoi(startPos, midPos, endPos, height - 1);
+		move(height - 1, endPos);
+		hanoi(midPos, endPos, startPos, height - 1);
+	} 
+} 
+
+function runIt() {	
+	var towersNum = document.getElementById("towers").value;
+	if(towersNum > 20) return false;
+	var prompt = document.getElementById("prompt");
+	initialize(towersNum);
+	hanoi('1','3','2',towersNum);	
+	prompt.style.opacity = "0";
+	setTimeout(function() {
+		prompt.style.display = "none";
+	}, 1000);
+}
